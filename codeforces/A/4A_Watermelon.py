e604073e2effc2b55fa7d213b8f2ccab980458d2
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-07-29 03:37:03 -0400

    4A_Watermelon.py

--- codeforces/A/4A_Watermelon.py
+++ codeforces/A/4A_Watermelon.py
@@ -16,3 +16,12 @@ else:
 #Method2:
 w = int(input())
 print("YES" if w % 2 == 0 and w > 2 else "NO")
+
+
+"""
+Solution Explanation:
+To divide the watermelon into two even parts with at least one piece each, 
+the weight must be even and greater than 2. Number 2 doesn't satisfy this.
+
+برای اینکه هندوانه بین دو نفر تقسیم بشه و هر کدوم حداقل یک قسمت زوج رو بگیرن باید وزن هندوانه زوج و بزرگتر از 2 باشه چون وزن 2 امکان تقسیم شدن به دو قسمت زوج رو نداره
+"""
