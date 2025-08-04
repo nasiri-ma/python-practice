1e9608ad218635d7266a519a2c77d333343c26a8
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-04 17:41:10 -0400

    Loops.py

---
+++ hackerrank/Easy/Loops.py
@@ -0,0 +1,9 @@
+#Problem: Loops
+#Link: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i ** 2)
