b54cab3d794a9caae1b072d535c2026693289540
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-04 17:27:47 -0400

    Say "Hello, World!" With Python.py

---
+++ hackerrank/Easy/Say "Hello, World!" With Python.py
@@ -0,0 +1,7 @@
+#problem: Say "Hello, World!" With Python
+Link: https://www.hackerrank.com/challenges/py-hello-world?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    print("Hello, World!")
