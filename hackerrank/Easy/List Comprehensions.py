9d420c27b6566f725757f0984102112b65c4639b
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-04 17:47:40 -0400

    List Comprehensions.py

---
+++ hackerrank/Easy/List Comprehensions.py
@@ -0,0 +1,17 @@
+#Problem: List Comprehensions
+#Link: https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    l = []
+    for i in range(x + 1):
+        for j in range(y + 1):
+            for k in range(z + 1):
+                if i + j + k != n:
+                    l.append([i, j, k])
+    print(l)
