289ac9437fdd8db102157d2ea585d22291298c61
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-04 17:37:41 -0400

    Arithmetic Operators.py

---
+++ hackerrank/Easy/Arithmetic Operators.py
@@ -0,0 +1,11 @@
+#Problem: Arithmetic Operators
+#Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
