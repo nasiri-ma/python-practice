52fe5ea994f05c69c7b89b8011847326d55b3f96
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-04 17:40:00 -0400

    Python: Division.py

---
+++ hackerrank/Easy/Python: Division.py
@@ -0,0 +1,10 @@
+#Problem: Python: Division
+#Link: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
