03af0ffdd3887edc50758b512bb390f730e69f83
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-05 11:22:35 -0400

    List Comprehensions.py

--- hackerrank/Easy/List Comprehensions.py
+++ hackerrank/Easy/List Comprehensions.py
@@ -2,7 +2,24 @@
 #Link: https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
 
 
-#Answer:
+#Method1:
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    result = [
+        [i, j, k]
+        for i in range(x + 1)
+        for j in range(y + 1)
+        for k in range(z + 1)
+        if i + j + k != n
+    ]
+    print(result)
+
+
+#Method2:
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
