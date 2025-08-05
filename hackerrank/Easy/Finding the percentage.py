d291b8daff6b234004669a2f5936ad827146b33c
Mohammad Amin Nasiri <211139297+nasiri-ma@users.noreply.github.com>
2025-08-05 06:05:35 -0400

    Finding the percentage.py

---
+++ hackerrank/Easy/Finding the percentage.py
@@ -0,0 +1,18 @@
+#Problem: Finding the percentage
+#Link: https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true
+
+
+#Answer:
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    scores = student_marks[query_name]
+    average = sum(scores) / len(scores)
+    print(f"{average:.2f}")
+  
